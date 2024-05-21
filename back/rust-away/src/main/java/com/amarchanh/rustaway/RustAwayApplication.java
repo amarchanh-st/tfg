157eaad6ac0299782ec924225c241eb6790144cd
antonio.marchante <antonio.marchante@ext.healthincode.com>
2024-05-21 19:58:53 +0200

    Back Archetype Generation

---
+++ back/rust-away/src/main/java/com/amarchanh/rustaway/RustAwayApplication.java
@@ -0,0 +1,13 @@
+package com.amarchanh.rustaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RustAwayApplication.class, args);
+	}
+
+}
